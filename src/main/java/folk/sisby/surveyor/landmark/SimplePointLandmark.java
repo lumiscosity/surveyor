8ddeb2191f870ad0c4928d6faed181332b10aa8b
lumiscosity <averyrudelphe@gmail.com>
2025-02-03 20:44:28 +0100

    replace description field with componentmap
    See issue #68.
    Implementation details are free to change here. I'm just throwing this out here while I'm testing it. Might add a ComponentMap for chunk summaries as well later, as mentioned in the issue.
    
    I don't know a thing about NBT, hence no 1.20 backport yet...

--- src/main/java/folk/sisby/surveyor/landmark/SimplePointLandmark.java
+++ src/main/java/folk/sisby/surveyor/landmark/SimplePointLandmark.java
@@ -2,6 +2,7 @@ package folk.sisby.surveyor.landmark;
 
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import folk.sisby.surveyor.Surveyor;
+import net.minecraft.component.ComponentMap;
 import net.minecraft.text.Text;
 import net.minecraft.text.TextCodecs;
 import net.minecraft.util.DyeColor;
@@ -12,20 +13,20 @@ import net.minecraft.util.math.BlockPos;
 import java.util.Optional;
 import java.util.UUID;
 
-public record SimplePointLandmark(BlockPos pos, Optional<UUID> optionalOwner, Optional<DyeColor> optionalColor, Optional<Text> optionalName, Optional<Text> optionalDescription, Optional<Identifier> optionalTexture) implements VariableLandmark<SimplePointLandmark> {
+public record SimplePointLandmark(BlockPos pos, Optional<UUID> optionalOwner, Optional<DyeColor> optionalColor, Optional<Text> optionalName, Optional<ComponentMap> optionalComponents, Optional<Identifier> optionalTexture) implements VariableLandmark<SimplePointLandmark> {
 	public static final LandmarkType<SimplePointLandmark> TYPE = new SimpleLandmarkType<>(
 		Identifier.of(Surveyor.ID, "point"),
 		pos -> RecordCodecBuilder.create(instance -> instance.group(
 			Uuids.CODEC.optionalFieldOf("owner").forGetter(VariableLandmark::optionalOwner),
 			DyeColor.CODEC.optionalFieldOf("color").orElse(null).forGetter(VariableLandmark::optionalColor),
 			TextCodecs.CODEC.optionalFieldOf("name").orElse(null).forGetter(VariableLandmark::optionalName),
-			TextCodecs.CODEC.optionalFieldOf("description").orElse(null).forGetter(VariableLandmark::optionalDescription),
+			ComponentMap.CODEC.optionalFieldOf("components").orElse(null).forGetter(VariableLandmark::optionalComponents),
 			Identifier.CODEC.optionalFieldOf("texture").orElse(null).forGetter(VariableLandmark::optionalTexture)
-		).apply(instance, (owner, color, name, description, texture) -> new SimplePointLandmark(pos, owner, color, name, description, texture)))
+		).apply(instance, (owner, color, name, components, texture) -> new SimplePointLandmark(pos, owner, color, name, components, texture)))
 	);
 
-	public SimplePointLandmark(BlockPos pos, UUID owner, DyeColor color, Text name, Text description, Identifier texture) {
-		this(pos, Optional.ofNullable(owner), Optional.ofNullable(color), Optional.ofNullable(name), Optional.ofNullable(description), Optional.ofNullable(texture));
+	public SimplePointLandmark(BlockPos pos, UUID owner, DyeColor color, Text name, ComponentMap components, Identifier texture) {
+		this(pos, Optional.ofNullable(owner), Optional.ofNullable(color), Optional.ofNullable(name), Optional.ofNullable(components), Optional.ofNullable(texture));
 	}
 
 	@Override
